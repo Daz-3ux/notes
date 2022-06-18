24665fd978c001c3a47d1f5f9eb6ee1cd62d6a19
Daz-3ux <1293821619@qq.com>
2022-06-18 16:38:25 +0800

    study

--- interset/c_fak/template.cc
+++ interset/c_fak/template.cc
@@ -7,11 +7,24 @@ void Print(T value)
     std::cout << value << std::endl;
 }
 
+template<typename T, int N>
+class Array
+{
+private:
+    T m_Array[N];
+public:
+    int GetSize() const { return N; }
+};
+
 int main(int argc, char** argv)
 {
-    Print("5");
+    Print(5);
+    Print<int>(6);
     Print("Hello,World");
-    Print("5.5f");
+    Print(5.5f);
+
+    Array<std::string, 1008611> array;
+    std::cout << array.GetSize() << std::endl;
 
     std::cin.get();
 }
