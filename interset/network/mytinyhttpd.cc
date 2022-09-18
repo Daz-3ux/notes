59fa5815cb7320757e18f9f2a6d6bc4cc8a245c9
Daz-3ux <1293821619@qq.com>
2022-09-18 16:07:48 +0800

    http

--- interset/network/mytinyhttpd.cc
+++ interset/network/mytinyhttpd.cc
@@ -27,13 +27,38 @@ void *accept_request(void *);
 
 int startup(u_short *);
 void error_die(const char *);
+void *accept_request(void *);
 
 
+/*
+此函数在一个特定端口启动监听网页连接的进程
+如果初始端口号是0,将动态分配一个端口并且改变原始端口变量(传入传出参数),以反应真实的端口号
+参数:指向包含要连接的端口的变量的指针
+返回值:socket端口号
+*/
+int startup(u_short *port)
+{
 
+}
 
+/*
+一个请求导致服务器端口上的accept()调用返回
+在正确的时机处理请求
+变量:连接到客户端的socket
+*/
+void *accept_request(void *client1)
+{
 
+}
 
+/*
+使用perror打印错误信息(针对系统错误;基于errno变量值确定系统调用错误)
+以及当检测到错误时退出进程
+*/
+void error_die(const char *sc)
+{
 
+}
 
 
 int main(int argc, char *argv[])
