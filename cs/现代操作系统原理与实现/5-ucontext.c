ee4e172ac74086258a74a8d610232c1bd4a40b9b
Daz-3ux <1293821619@qq.com>
2022-10-07 21:23:51 +0800

    os

---
+++ cs/现代操作系统原理与实现/5-ucontext.c
@@ -0,0 +1,43 @@
+#include <signal.h>
+#include <stdio.h>
+#include <ucontext.h>
+#include <unistd.h>
+
+ucontext_t ucontext1, ucontext2;
+
+int current = 0;
+
+void produce()
+{
+  sleep(0.5); // 为了可以直观观看
+  current++;
+  setcontext(&ucontext2);
+}
+
+void consume()
+{
+  printf("current value: %d\n", current);
+  setcontext(&ucontext1);
+}
+
+int main(int argc, char **argv)
+{
+  char iterator_stack1[SIGSTKSZ];
+  char iterator_stack2[SIGSTKSZ];
+
+  getcontext(&ucontext1);
+  ucontext1.uc_link = NULL;
+  ucontext1.uc_stack.ss_sp = iterator_stack1;
+  ucontext1.uc_stack.ss_size = sizeof(iterator_stack1);
+  makecontext(&ucontext1, (void (*)(void))produce, 0);
+
+  getcontext(&ucontext2);
+  ucontext2.uc_link = NULL;
+  ucontext2.uc_stack.ss_sp = iterator_stack2;
+  ucontext2.uc_stack.ss_size = sizeof(iterator_stack2);
+  makecontext(&ucontext2, (void(*)(void))consume, 0);
+
+  setcontext(&ucontext1);
+
+  return 0;
+}
