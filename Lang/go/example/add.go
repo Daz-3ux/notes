f4e8c0191855bedd5f76b306941d7e19d6af997f
Daz-3ux <1293821619@qq.com>
2023-03-03 22:55:34 +0800

    init go

--- Lang/go/example/add.go
+++ Lang/go/example/add.go
@@ -1,27 +1,27 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"strings"
-)
+// import (
+// 	"fmt"
+// 	"strings"
+// )
 
-func main() {
-	board := [][]string{
-		[]string{"_", "_", "_"},
-		[]string{"_", "_", "_"},
-		[]string{"_", "_", "_"},
-	}
+// func main() {
+// 	board := [][]string{
+// 		[]string{"_", "_", "_"},
+// 		[]string{"_", "_", "_"},
+// 		[]string{"_", "_", "_"},
+// 	}
 
-	board[0][0] = "X"
-	board[2][2] = "O"
-	board[1][2] = "X"
-	board[1][0] = "O"
-	board[0][2] = "X"
-	board[1][1] = "O"
+// 	board[0][0] = "X"
+// 	board[2][2] = "O"
+// 	board[1][2] = "X"
+// 	board[1][0] = "O"
+// 	board[0][2] = "X"
+// 	board[1][1] = "O"
 
-	fmt.Println(len(board))
+// 	fmt.Println(len(board))
 
-	for i := 0; i < len(board); i++ {
-		fmt.Printf("%s\n", strings.Join(board[i], " "))
-	}
-}
+// 	for i := 0; i < len(board); i++ {
+// 		fmt.Printf("%s\n", strings.Join(board[i], " "))
+// 	}
+// }
