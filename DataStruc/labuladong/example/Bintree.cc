0fa1ed822d018449dd5d95aff164347d9420a019
Daz-3ux <1293821619@qq.com>
2023-03-09 13:33:40 +0800

    reset

--- DataStruc/labuladong/example/Bintree.cc
+++ DataStruc/labuladong/example/Bintree.cc
@@ -3,37 +3,26 @@
 
 using namespace std;
 
-template <typename T> 
+template <typename T>
 struct Stack {
 public:
   Stack() = default;
 
-  void push(const T& val) { 
-    data_.push_back(val);
-  }
+  void push(const T &val) { data_.push_back(val); }
 
-  void pop() {
-    data_.pop_back();
-  }
+  void pop() { data_.pop_back(); }
 
-  const T& top() const {
-    return data_.back();
-  }
+  const T &top() const { return data_.back(); }
 
-  bool empty() const {
-    return data_.empty();
-  }
+  bool empty() const { return data_.empty(); }
 
-  size_t size() const {
-    return data_.size();
-  }
-
+  size_t size() const { return data_.size(); }
 
 private:
   vector<T> data_;
 };
 
-template <typename T> 
+template <typename T>
 struct BinTreeNode {
   T data;
   BinTreeNode *LeftChild;
@@ -44,7 +33,7 @@ struct BinTreeNode {
 // 二叉树先序遍历 递归版
 template <typename T>
 void travPre_R(BinTreeNode<T> *root) {
-  if(!root) {
+  if (!root) {
     cout << "panic" << endl;
     return;
   }
@@ -56,54 +45,78 @@ void travPre_R(BinTreeNode<T> *root) {
 // 二叉树先序遍历 迭代版
 template <typename T>
 void travPre_v1(BinTreeNode<T> *root) {
-  Stack<BinTreeNode<T> *> s; // stack for iteration
-  if(root)
-    s.push(root);
+  Stack<BinTreeNode<T> *> s;  // stack for iteration
+  if (root) s.push(root);
   while (!s.empty()) {
     root = s.pop();
     cout << root->data;
 
-    if(root->RightChild)
-      s.push(root->RightChild); // right child first in last out
-    if(root->LeftChild) 
+    if (root->RightChild)
+      s.push(root->RightChild);  // right child first in last out
+    if (root->LeftChild)
       s.push(root->LeftChild);  // left child last in first out
   }
 }
 
 // 二叉树先序遍历 迭代版 Plus
 template <typename T>
-static void VisitAlongLeftBranch(BinTreeNode<T> *x, Stack<BinTreeNode<T> *> &S) {
-  while(x) {
+static void VisitAlongLeftBranch(BinTreeNode<T> *x,
+                                 Stack<BinTreeNode<T> *> &S) {
+  while (x) {
     cout << x->data;
-    if(x->RightChild)
-      S.push(x -> RightChild);
+    if (x->RightChild) S.push(x->RightChild);
     x = x->LeftChild;
   }
 }
 
 template <typename T>
-void travPre_v2(BinTreeNode<T>* root) {
-  Stack<BinTreeNode<T>* > S;
-  while(true) {
+void travPre_v2(BinTreeNode<T> *root) {
+  Stack<BinTreeNode<T> *> S;
+  while (true) {
     VisitAlongLeftBranch(root, S);
-    if(S.empty())
-      break;
+    if (S.empty()) break;
     root = S.pop();
   }
 }
 
 // 二叉树中序遍历 递归实现
 template <typename T>
-void travIn_R(BinTreeNode<T>* root) {
-  if(!root) 
-    return;
+void travIn_R(BinTreeNode<T> *root) {
+  if (!root) return;
   travIn_R(root->LeftChild);
   cout << root->data;
   travIn_R(root->RightChild);
 }
 
-
-int main() {
-
-  return 0;
+// 二叉树中序遍历 迭代实现
+template <typename T>
+void travIn_v2(BinTreeNode<T> *root) {
+  Stack<BinTreeNode<T> *> S;
+  while (true) {
+    if (root) {
+      S.push(root);
+      root = root->LeftChild;
+    } else if (!S.empty()) {
+      root = S.pop();
+      cout << root->data;
+      root = root->RightChild;
+    } else {
+      break;
+    }
+  }
 }
+
+// 后序遍历的 递归实现
+template <typename T>
+void travPost_R(BinTreeNode<T> *root) {
+  if(!root) return;
+  travPost_R(root->LeftChild);
+  travPost_R(root->RightChild);
+  cout << root->data;
+}
+
+// 层次遍历
+template <typename T>
+void travLevelk
+
+int main() { return 0; }
