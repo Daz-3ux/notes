33eb1c03b52a30a11dbdbe41f095d16bc885695a
Pengda Yang <daz-3ux@proton.me>
2023-11-15 22:13:12 +0800

    docs: update notes
    Signed-off-by: Pengda Yang <daz-3ux@proton.me>

--- course/test.go
+++ course/test.go
@@ -1,25 +1,56 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+    "context"
+    "fmt"
+    "sync"
+    "time"
 )
 
+func PrintOdd(ctx context.Context, wg *sync.WaitGroup, num int, odd, even chan struct{}) {
+    defer wg.Done()
+    for {
+        select {
+        case <-ctx.Done():
+            return
+        case <-odd:
+            fmt.Println(num)
+            num += 2
+            time.Sleep(time.Millisecond * 500)
+            even <- struct{}{}
+        }
+    }
+}
+
+func PrintEven(ctx context.Context, wg *sync.WaitGroup, num int, odd, even chan struct{}) {
+    defer wg.Done()
+    for {
+        select {
+        case <-ctx.Done():
+            return
+        case <-even:
+            fmt.Println(num)
+            num += 2
+            time.Sleep(time.Millisecond * 500)
+            odd <- struct{}{}
+        }
+    }
+}
 
 func main() {
-	ch := make(chan int, 3);
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 3; i++ {
-			ch <- i
-		}
-		close(ch)
-	}()
+    var wg sync.WaitGroup
+    num1 := 1
+    num2 := 0
+    oddchan := make(chan struct{}, 1)
+    evenchan := make(chan struct{}, 1)
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+    defer cancel()
 
-	for i := 0; i < 10; i++ {
-		data, ok := <- ch
-		fmt.Println(data, ok)
-	}
+    wg.Add(2)
+    go PrintEven(ctx, &wg, num2, oddchan, evenchan)
+    go PrintOdd(ctx, &wg, num1, oddchan, evenchan)
+
+    evenchan <- struct{}{}
+    wg.Wait()
+
 }
